bd7321565ca86ad07f51fe96b99d013dacb28db1
gihanrcg <it17016230@my.sliit.lk>
2019-09-01 16:16:06 +0530

    Live webcase added

--- app/src/main/java/fragment/WebcastFragment.java
+++ app/src/main/java/fragment/WebcastFragment.java
@@ -6,7 +6,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,7 +20,7 @@ import com.example.srilankacricketappv1.R;
 public class WebcastFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
 
-    private ImageView imageView;
+    private WebView webView;
 
 
     public WebcastFragment() {
@@ -30,26 +34,14 @@ public class WebcastFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_webcast, container, false);
-        imageView = view.findViewById(R.id.imageView_webcast);
-        setOnClickListners();
+        webView = view.findViewById(R.id.youtubeView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.youtube.com/watch?v=XSmcBKdtA5A");
         return view;
 
     }
 
-    private void setOnClickListners() {
 
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=7Xz9Xv1CRnA&t=5s"));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.setPackage("com.google.android.youtube");
-                startActivity(intent);
-            }
-        });
-
-
-    }
 }
