2cf1e21e2b769e73df0ed9fbbb5df9c24fdb1936
nishitha97 <nishithadesilva123@gmail.com>
2019-09-01 15:47:21 +0530

    added youtube link and icon to webcast

--- app/src/main/java/fragment/WebcastFragment.java
+++ app/src/main/java/fragment/WebcastFragment.java
@@ -1,14 +1,14 @@
 package fragment;
 
-import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.srilankacricketappv1.R;
 
@@ -16,21 +16,40 @@ import com.example.srilankacricketappv1.R;
 public class WebcastFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
 
+    private ImageView imageView;
+
 
     public WebcastFragment() {
         // Required empty public constructor
     }
 
-
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_webcast, container, false);
+        View view = inflater.inflate(R.layout.fragment_webcast, container, false);
+        imageView = view.findViewById(R.id.imageView_webcast);
+        setOnClickListners();
+        return view;
+
     }
 
+    private void setOnClickListners() {
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=7Xz9Xv1CRnA&t=5s"));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setPackage("com.google.android.youtube");
+                startActivity(intent);
+            }
+        });
 
 
-
+    }
 }
