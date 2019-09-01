8dddfca8a4262ead14b41c4fe3010f415e811379
lmvithanage <sanjayajayasinghe54@gmail.com>
2019-09-01 14:42:35 +0530

    add map view

--- app/src/main/java/fragment/ContactFragment.java
+++ app/src/main/java/fragment/ContactFragment.java
@@ -1,5 +1,6 @@
 package fragment;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -14,6 +15,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.srilankacricketappv1.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.w3c.dom.Text;
 
@@ -26,6 +36,20 @@ public class ContactFragment extends Fragment {
     TextView email;
     TextView contactNumber;
     TextView web;
+    private static final String[] INITIAL_PERMS={
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_CONTACTS
+    };
+    private static final String[] LOCATION_PERMS={
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    private static final int INITIAL_REQUEST=1337;
+    private static final int CAMERA_REQUEST=INITIAL_REQUEST+1;
+    private static final int CONTACTS_REQUEST=INITIAL_REQUEST+2;
+    private static final int LOCATION_REQUEST=INITIAL_REQUEST+3;
+    MapView mMapView;
+    private GoogleMap googleMap;
 
     public ContactFragment() {
         // Required empty public
@@ -36,14 +60,84 @@ public class ContactFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_contact, container, false);
-        email = view.findViewById(R.id.email);
-        contactNumber = view.findViewById(R.id.contactNumber);
-        web = view.findViewById(R.id.web);
+        if (!canAccessLocation() || !canAccessContacts()) {
+            requestPermissions(INITIAL_PERMS, INITIAL_REQUEST);
+        }
+        View rootView = inflater.inflate(R.layout.fragment_contact, container, false);
+        email = rootView.findViewById(R.id.email);
+        contactNumber = rootView.findViewById(R.id.contactNumber);
+        web = rootView.findViewById(R.id.web);
         addClickListners();
+        //setContentView(R.layout.activity_maps);
 
 
-        return view;
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+
+                googleMap.setMyLocationEnabled(true);
+
+
+                LatLng sydney = new LatLng(6.906266,  79.868301);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+
+
+
+
+
+        return rootView;
+    }
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 
     private void addClickListners() {
@@ -83,6 +177,23 @@ public class ContactFragment extends Fragment {
                 startActivity(browserIntent);
             }
         });
+    }
+
+    private boolean canAccessLocation() {
+        return(hasPermission(Manifest.permission.ACCESS_FINE_LOCATION));
+    }
+
+    private boolean canAccessCamera() {
+        return(hasPermission(Manifest.permission.CAMERA));
+    }
+
+    private boolean canAccessContacts() {
+        return(hasPermission(Manifest.permission.READ_CONTACTS));
+    }
+
+    private boolean hasPermission(String perm) {
+       // return(PackageManager.PERMISSION_GRANTED==checkSelfPermission(perm));
+        return false;
     }
 
 }
--- app/src/main/java/fragment/PollingFragment.java
+++ app/src/main/java/fragment/PollingFragment.java
@@ -1,16 +1,27 @@
 package fragment;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.example.srilankacricketappv1.R;
+
+import java.io.InputStream;
 
 
 public class PollingFragment extends Fragment {
@@ -25,7 +36,55 @@ public class PollingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_polling, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String[] arraySpinner = new String[] {
+                "sl vs nz 1st test match", "2", "3", "4", "5", "6", "7"
+        };
+        Spinner s = (Spinner) getView().findViewById(R.id.current_matches_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+
+        new DownloadImageTask((ImageView) getView().findViewById(R.id.country1_logo))
+                .execute("https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Flag_of_Sri_Lanka.svg/1920px-Flag_of_Sri_Lanka.svg.png");
+        new DownloadImageTask((ImageView) getView().findViewById(R.id.country2_logo))
+                .execute("https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/383px-Flag_of_India.svg.png");
+
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 
 
